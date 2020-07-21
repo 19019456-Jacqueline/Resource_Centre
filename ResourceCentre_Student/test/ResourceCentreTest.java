197b8383cff8ff159711f667e9e9e05e0041698f
19025607 <19025607@DESKTOP-HDIO95N.mshome.net>
2020-07-21 14:33:26 +0800

    Code done for retrieveAllChromebookTest

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -81,7 +81,23 @@ public class ResourceCentreTest {
 	@Test
 	public void retrieveAllChromebookTest() {
 		//fail("Not yet implemented");
-		// write your code here
+		// write your code here (eukuang)
+		assertNotNull("Test if there is valid Camcorder arraylist to add to", chromebookList);
+		String allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
+		
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test if that Chromebook arraylist size is 2?", 2, chromebookList.size());
+		
+		allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+		
+		testOutput = String.format("%-10s %-30s %-10s %-10s %-20s\n", "My Google Chromebook list", "Yes", "", "Mac 05");
+		testOutput += String.format("%-10s %-30s %-10s %-10s %-20s\n", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
+		
+		assertEquals("Check that ViewAllCamcorderlist", testOutput, allChromebook);
+		
 	}
 
 	@Test
